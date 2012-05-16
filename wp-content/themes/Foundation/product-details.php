7fbd2ebc8a5032f7bb2db78c221aca8bf5ee373d
Nathan <nathan@nathanfeaver.com>
2012-05-16 13:01:23 -0600

    added YUI TabView and Navigation

---
+++ wp-content/themes/Foundation/product-details.php
@@ -0,0 +1,15 @@
+<div id="demo" class="yui-navset">
+    <ul class="yui-nav">
+        <li class="selected"><a href="#tab1"><em>Specifications</em></a></li>
+        <li><a href="#tab2"><em>Reviews</em></a></li>
+        <li><a href="#tab3"><em>Warranty</em></a></li>
+    </ul>            
+    <div class="yui-content">
+        <div><p>Tab One Content</p></div>
+        <div><p>Tab Two Content</p></div>
+        <div><p>Tab Three Content</p></div>
+    </div>
+</div>
+<script type="text/javascript">
+  var myTabs = new YAHOO.widget.TabView("demo");
+</script>
--- wp-content/themes/Foundation/sidebar-related-navigation.php
+++ wp-content/themes/Foundation/sidebar-related-navigation.php
@@ -1,3 +1,27 @@
-
-  <h5>Related Navigation</h5>
-
+<h5>Related Navigation</h5>
+<ul>
+  <li>
+    Category
+    <ul>
+      <li>Sub-Category 1</li>
+      <li>Sub-Category 2</li>
+      <li>Sub-Category 3</li>
+    </ul>
+  </li>
+  <li>
+    Category 1
+    <ul>
+      <li>Sub-Category 1</li>
+      <li>Sub-Category 2</li>
+      <li>Sub-Category 3</li>
+    </ul>
+  </li>
+  <li>
+    Category 2
+    <ul>
+      <li>Sub-Category 1</li>
+      <li>Sub-Category 2</li>
+      <li>Sub-Category 3</li>
+    </ul>
+  </li>
+</ul>
