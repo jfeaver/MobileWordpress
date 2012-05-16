2687e70ab8a552312ed55b69df31b3379c83a565
Nathan <nathan@nathanfeaver.com>
2012-05-15 22:30:11 -0600

    template parts set up

---
+++ wp-content/themes/Foundation/related-products.php
@@ -0,0 +1,12 @@
+<div class="two columns">
+  <ul id="related_product_list">
+    <li>
+      <img src="./wp-content/uploads/2012/05/baconsinglecurl.jpg"/>
+    </li>
+    <li>
+      <img src="./wp-content/uploads/2012/05/baconsinglecurl.jpg"/>
+    </li>
+    <li>
+      <img src="./wp-content/uploads/2012/05/baconsinglecurl.jpg"/>
+    </li>
+</div>
